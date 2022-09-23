e463585e791b4032a52209280fcdfdd5ca29da96
Tanmay Bhosale <tanmaybhosale24@gmail.com>
2022-09-23 11:54:35 +0530

    iitial commit

---
+++ pages/about.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Meta from "../components/Meta";
+
+function About(){
+    return(
+        <div>
+            <Meta title="About" />
+            This is about
+        </div>
+    )
+}
+
+export default About
